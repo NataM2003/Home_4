e93d0cbaa43e7c176ac2222fe24fdb5879dc0785
NataM2003 <91798703+NataM2003@users.noreply.github.com>
2021-11-02 11:55:12 +0200

    Create project

---
+++ Homework_4/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Homework_4
+{
+   public class Person
+    {
+        private string name;
+        private DateTime birthYear;
+
+        public string Name { get { return name; } }
+        public DateTime BirthYear { get { return birthYear; } }
+
+        public Person()
+        {
+            name = "Geralt";
+            birthYear = new DateTime(2000, 12, 12);
+        }
+        public Person(string _Name, int _BirthYear)
+        {
+            name = _Name;
+            birthYear = new DateTime(_BirthYear, 12, 12);
+        }
+
+        public int Age()
+        {
+            return DateTime.Now.Year - birthYear.Year;
+        }
+
+        public void Input()
+        {
+            Console.Write("Enter person name:");
+            name = Console.ReadLine();
+            Console.Write("Enter the year of birth: ");
+            birthYear = new DateTime(Convert.ToInt32(Console.ReadLine()), 12, 12);
+        }
+        public void ChangeName(string _Name)
+        {
+            name = _Name;
+        }
+
+        public override string ToString()
+        {
+            return "Name: " + Name + ".\nAge: " + Age();
+        }
+
+        public void Output()
+        {
+            Console.WriteLine(ToString());
+            Console.WriteLine(" ");
+        }
+        public static bool operator ==(Person first, Person second)
+        {
+            return first.name == second.name;
+        }
+
+        public static bool operator !=(Person first, Person second)
+        {
+            return !(first == second);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+    }
+}
---
+++ Homework_4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[6];
+
+            Console.WriteLine("Enter name and birth year");
+
+            for (int i = 0; i < 6; i++)
+            {
+                persons[i] = new Person();
+
+                Console.WriteLine($"Person {i + 1}");
+                persons[i].Input();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Info :");
+
+            foreach (Person p in persons)
+            {
+                p.Output();
+            }
+
+            Console.WriteLine("Updated information:\n");
+
+            foreach (Person p in persons)
+            {
+
+                if (p.Age() < 16)
+                {
+                    p.ChangeName("Very Young\n");
+                }
+
+                p.Output();
+                Console.WriteLine("\nPeople with same names:\n");
+
+            Dictionary<string, int> some_names =
+                new Dictionary<string, int>();
+
+            foreach (Person pers in persons)
+            {
+                string name = pers.Name;
+
+                if (some_names.ContainsKey(name))
+                {
+                    some_names[name]++;
+                }
+                else
+                {
+                    some_names.Add(name, 1);
+                }
+            }
+
+            foreach (Person pers in persons)
+            {
+                if (some_names[pers.Name] >= 2)
+                {
+                    pers.Output();
+                }
+            }
+            }
+        }
+    }
+}
